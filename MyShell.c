e685553a760f735b76f454cdf04404cb6c4d4156
Soham De <de.soham004@gmail.com>
2025-02-08 14:34:12 +0000

    Added prototype execute function after tokenizing the input

--- MyShell.c
+++ MyShell.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <linux/limits.h>
+#include <sys/wait.h>
 #include <ctype.h>
 
 int DEBUG = 0;
@@ -117,6 +118,7 @@ char **shell_split(char *line){
 
         // Store the token
         tokens[position] = strdup(token_start);
+        printf(DEBUG ? "%s[%d]\n" : "", token_start,position);
         position++;
 
         // Resize token array if needed
@@ -129,12 +131,111 @@ char **shell_split(char *line){
             }
         }
 
-        printf(DEBUG ? "%s\n" : "", token_start);
+        
     }
 
     tokens[position] = NULL; // Null terminate the token array
     return tokens;
     
+}
+
+
+int shell_launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } 
+  else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } 
+  else {
+    // Parent process
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
+
+int shell_cd(char **args);
+int shell_help(char **args);
+int shell_exit(char **args);
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+int (*builtin_func[]) (char **) = {
+    &shell_cd,
+    &shell_help,
+    &shell_exit
+};
+
+int shell_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int shell_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } 
+  else {
+    printf(DEBUG ? "Changing dir to: %s \n" : "", args[1]);
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+int shell_help(char **args)
+{
+  int i;
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < shell_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int shell_exit(char **args)
+{
+  return 0;
+}
+
+int shell_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < shell_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return shell_launch(args);
 }
 
 void shell_loop(){
@@ -152,15 +253,13 @@ void shell_loop(){
         line = shell_read_line();
         printf(DEBUG?"Line: %s \n":"", line);
         args = shell_split(line);
-        
+        status = shell_execute(args);
         free(line);
         free(args);
     }while(status);
     
 
 }
-
-
 
 int main(int argc, char const *argv[])
 {
